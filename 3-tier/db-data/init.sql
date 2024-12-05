671231c659a93d442e6b8fb68c955abb072b752e
root <root@ip-172-31-4-172.ap-south-1.compute.internal>
2024-12-05 13:17:11 +0000

    Revert "adding new files"
    This reverts commit bcefdf56f014048310dab54fc713c8d999bce040.

--- 3-tier/db-data/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE registration_db;
-USE registration_db;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
-);
